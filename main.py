fdba7be7c28ef3b6eff309d34580251ff8dc7988
AdamKostoev <125017809+AdamKostoev@users.noreply.github.com>
2023-05-25 06:59:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import requests
+city = "Magas,RU"
+appid = "ba436182fe02ac5a3196306dd4da2de2"
+res = requests.get("http://api.openweathermap.org/data/2.5/weather",
+      params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Город:", city)
+print("Температура:", data['main']['temp'])
+print("Минимальная температура:", data['main']['temp_min'])
+print("Максимальная температура ", data['main']['temp_max'])
+print("Скорость ветра", data['wind']['speed'])
+print("Видимость", data['visibility'])
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                   params={'q': city, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+data = res.json()
+print("Прогноз на неделю:")
+for i in data['list']:
+      print("Дата <", i['dt_txt'], "> \r\nТемпература <", '{0:+3.0f}'.format(i['main']['temp']),
+            "> \r\nПогодные условия <", i['weather'][0]['description'], ">", " \r\nСкорость ветра < > ")
+      print("Скорость ветра <", i['wind']['speed'], "> \r\nВидимость <", i['visibility'], ">")
+      print("_____________________________________")
